74c0e96c2086fbf0ea9d0f50c46db23df364e933
lucioaguiar <lucioaguiardesouza@gmail.com>
2026-09-01 17:17:26 -0300

    FEAT[1] - Adicionada opção de exclusão de usuário

--- src/SistemaCadastro.java
+++ src/SistemaCadastro.java
@@ -7,6 +7,7 @@ public class SistemaCadastro {
         System.out.println("=== Sistema de Cadastro ===");
         System.out.println("1 - Cadastrar usuário");
         System.out.println("2 - Listar usuários");
+        System.out.println("3 - Excluir usuário");
         System.out.println("0 - Sair");
     }
 }
